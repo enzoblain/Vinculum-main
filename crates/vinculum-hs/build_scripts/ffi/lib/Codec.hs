3085c41f695f6370ffd8e4b9bf2decf478296b86
enzoblain <enzoblain@proton.me>
2026-04-02 22:05:00 +0200

    chore: only primitive types for now

--- crates/vinculum-hs/build_scripts/ffi/lib/Codec.hs
+++ crates/vinculum-hs/build_scripts/ffi/lib/Codec.hs
@@ -1,7 +1,6 @@
 module Codec where
 
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as C8
 import Data.Word
 import Data.Int
 import Data.Bits
@@ -20,10 +19,6 @@ data Value
     | VFloat64 Double
     | VBool Bool
     | VChar Char
-    | VString String
-    | VBytes BS.ByteString
-    | VOption (Maybe Value)
-    | VVec [Value]
 
 decodeValues :: BS.ByteString -> [Value]
 decodeValues bs
@@ -89,45 +84,8 @@ decodeOne bs =
                  in case toEnum (fromIntegral code) of
                         c -> (VChar c, remaining)
 
-            | tag == 12 ->
-                let (lenBytes, afterLen) = BS.splitAt 8 rest
-                    strLen = fromIntegral (bytesToWord64 lenBytes)
-                    (payload, remaining) = BS.splitAt strLen afterLen
-                 in (VString (C8.unpack payload), remaining)
-
-            | tag == 13 ->
-                let (lenBytes, afterLen) = BS.splitAt 8 rest
-                    rawLen = fromIntegral (bytesToWord64 lenBytes)
-                    (payload, remaining) = BS.splitAt rawLen afterLen
-                 in (VBytes payload, remaining)
-
-            | tag == 14 ->
-                case BS.uncons rest of
-                    Nothing -> error "Incomplete Option encoding"
-                    Just (optTag, remaining)
-                        | optTag == 0 -> (VOption Nothing, remaining)
-                        | optTag == 1 ->
-                            let (value, finalRemaining) = decodeOne remaining
-                             in (VOption (Just value), finalRemaining)
-                        | otherwise -> error ("Invalid Option tag: " ++ show optTag)
-
-            | tag == 15 ->
-                let (lenBytes, afterLen) = BS.splitAt 8 rest
-                    vecLen = fromIntegral (bytesToWord64 lenBytes)
-                    (vecElements, vecRemaining) = decodeNValues vecLen afterLen
-                 in (VVec vecElements, vecRemaining)
-
             | otherwise ->
                 error ("Invalid type tag: " ++ show tag)
-
-decodeNValues :: Int -> BS.ByteString -> ([Value], BS.ByteString)
-decodeNValues 0 bs = ([], bs)
-decodeNValues n bs
-    | n > 0 =
-        let (value, rest) = decodeOne bs
-            (restValues, finalRemaining) = decodeNValues (n - 1) rest
-         in (value : restValues, finalRemaining)
-    | otherwise = error "Invalid vector length"
 
 encodeInt8 :: Int8 -> BS.ByteString
 encodeInt8 x =
@@ -177,45 +135,6 @@ encodeChar :: Char -> BS.ByteString
 encodeChar c =
     BS.pack (11 : word32ToBytes (fromIntegral (fromEnum c)))
 
-encodeString :: String -> BS.ByteString
-encodeString s =
-    let bytes = C8.pack s
-        lenBytes = word64ToBytes (fromIntegral (BS.length bytes))
-     in BS.concat [BS.pack [12], BS.pack lenBytes, bytes]
-
-encodeBytes :: BS.ByteString -> BS.ByteString
-encodeBytes b =
-    let lenBytes = word64ToBytes (fromIntegral (BS.length b))
-     in BS.concat [BS.pack [13], BS.pack lenBytes, b]
-
-encodeOptionNothing :: BS.ByteString
-encodeOptionNothing =
-    BS.pack [14, 0]
-
-encodeOptionJust :: BS.ByteString -> BS.ByteString
-encodeOptionJust value =
-    BS.concat [BS.pack [14, 1], value]
-
-encodeOption :: Maybe Value -> BS.ByteString
-encodeOption Nothing =
-    encodeOptionNothing
-encodeOption (Just value) =
-    encodeOptionJust (encodeValue value)
-
-encodeOptionWith :: (a -> Value) -> Maybe a -> BS.ByteString
-encodeOptionWith _ Nothing = encodeOptionNothing
-encodeOptionWith f (Just x) = encodeOptionJust (encodeValue (f x))
-
-decodeOptionWith :: (Value -> a) -> Maybe Value -> Maybe a
-decodeOptionWith _ Nothing = Nothing
-decodeOptionWith f (Just v) = Just (f v)
-
-encodeVec :: [Value] -> BS.ByteString
-encodeVec values =
-    let lenBytes = word64ToBytes (fromIntegral (length values))
-        encodedValues = BS.concat (map encodeValue values)
-     in BS.concat [BS.pack [15], BS.pack lenBytes, encodedValues]
-
 encodeValue :: Value -> BS.ByteString
 encodeValue val = case val of
     VInt8 x -> encodeInt8 x
@@ -230,10 +149,6 @@ encodeValue val = case val of
     VFloat64 x -> encodeFloat64 x
     VBool b -> encodeBool b
     VChar c -> encodeChar c
-    VString s -> encodeString s
-    VBytes b -> encodeBytes b
-    VOption opt -> encodeOption opt
-    VVec v -> encodeVec v
 
 word8ToBytes :: Word8 -> [Word8]
 word8ToBytes w = [w]
@@ -301,59 +216,3 @@ bytesToWord64 bs =
             .|. (fromIntegral b6 `shiftL` 48)
             .|. (fromIntegral b7 `shiftL` 56)
         _ -> error "Expected exactly 8 bytes"
-
-fromValueInt8 :: Value -> Int8
-fromValueInt8 (VInt8 x) = x
-fromValueInt8 _ = error "Expected Int8"
-
-fromValueInt16 :: Value -> Int16
-fromValueInt16 (VInt16 x) = x
-fromValueInt16 _ = error "Expected Int16"
-
-fromValueInt32 :: Value -> Int32
-fromValueInt32 (VInt32 x) = x
-fromValueInt32 _ = error "Expected Int32"
-
-fromValueInt64 :: Value -> Int64
-fromValueInt64 (VInt64 x) = x
-fromValueInt64 _ = error "Expected Int64"
-
-fromValueWord8 :: Value -> Word8
-fromValueWord8 (VWord8 x) = x
-fromValueWord8 _ = error "Expected Word8"
-
-fromValueWord16 :: Value -> Word16
-fromValueWord16 (VWord16 x) = x
-fromValueWord16 _ = error "Expected Word16"
-
-fromValueWord32 :: Value -> Word32
-fromValueWord32 (VWord32 x) = x
-fromValueWord32 _ = error "Expected Word32"
-
-fromValueWord64 :: Value -> Word64
-fromValueWord64 (VWord64 x) = x
-fromValueWord64 _ = error "Expected Word64"
-
-fromValueFloat32 :: Value -> Float
-fromValueFloat32 (VFloat32 x) = x
-fromValueFloat32 _ = error "Expected Float32"
-
-fromValueFloat64 :: Value -> Double
-fromValueFloat64 (VFloat64 x) = x
-fromValueFloat64 _ = error "Expected Float64"
-
-fromValueBool :: Value -> Bool
-fromValueBool (VBool x) = x
-fromValueBool _ = error "Expected Bool"
-
-fromValueChar :: Value -> Char
-fromValueChar (VChar x) = x
-fromValueChar _ = error "Expected Char"
-
-fromValueString :: Value -> String
-fromValueString (VString x) = x
-fromValueString _ = error "Expected String"
-
-fromValueBytes :: Value -> BS.ByteString
-fromValueBytes (VBytes x) = x
-fromValueBytes _ = error "Expected Bytes"
